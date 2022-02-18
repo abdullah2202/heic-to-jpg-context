af5c40654ddcfc0b062c83faecaaafa4b4b0437b
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-02-18 17:52:25 +0000

    Working script

--- heictojpg.py
+++ heictojpg.py
@@ -7,5 +7,9 @@ directory = "."
 for filename in os.listdir(directory):
    if filename.lower().endswith(".heic"):
       print('Converting %s...' % os.path.join(directory, filename))
-      # subprocess.run(["magick", "%s" % filename, "%s" % (filename[0:-5] + '.jpg')])
+      subprocess.run(["magick", "%s" % filename, "%s" % (filename[0:-5] + '.jpg')])
+      # inputFile = filename
+      # outputFile = filename[0:-5] + '.jpg'
+      # os.system('cmd /k "magick %s %s"'%(inputFile, outputFile))
+      # os.system('cmd /k "echo %s"' %filename )
       continue
