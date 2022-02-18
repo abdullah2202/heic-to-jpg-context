88be3d82f49e66a40f0ba4a63e56e006fdaf5b59
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-02-18 16:10:29 +0000

    Create python file and demo image

---
+++ heictojpg.py
@@ -0,0 +1,11 @@
+
+from calendar import c
+import os, subprocess
+
+directory = "."
+
+for filename in os.listdir(directory):
+   if filename.lower().endswith(".heic"):
+      print('Converting %s...' % os.path.join(directory, filename))
+      # subprocess.run(["magick", "%s" % filename, "%s" % (filename[0:-5] + '.jpg')])
+      continue
