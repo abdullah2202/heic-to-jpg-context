1f429fa98b6bb1998942a0c66b2e5c9736247138
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-02-18 17:52:54 +0000

    removed unneccesary comments

--- heictojpg.py
+++ heictojpg.py
@@ -8,8 +8,4 @@ for filename in os.listdir(directory):
    if filename.lower().endswith(".heic"):
       print('Converting %s...' % os.path.join(directory, filename))
       subprocess.run(["magick", "%s" % filename, "%s" % (filename[0:-5] + '.jpg')])
-      # inputFile = filename
-      # outputFile = filename[0:-5] + '.jpg'
-      # os.system('cmd /k "magick %s %s"'%(inputFile, outputFile))
-      # os.system('cmd /k "echo %s"' %filename )
       continue
